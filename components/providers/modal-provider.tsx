cddb47d70f14f323a73781499785f8e3a014da71
AhmedRezeq8 <sherlock.am8@gmai.com>
2023-09-17 15:01:31 +0300

    Server Sidebar and Invitations

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,21 +1,31 @@
 "use client";
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
-  
-    useEffect(() => {
-      setIsMounted(true);
-    }, []);
-  
-    if (!isMounted) {
-      return null;
-    }
-  
-    return (
-      <>
-        <CreateServerModal />
-    
-      </>
-    )
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
   }
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      {/* <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal /> */}
+    </>
+  );
+};
