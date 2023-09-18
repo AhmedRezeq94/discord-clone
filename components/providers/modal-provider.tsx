5afbf5971736c684568d3defcbfc098bcd44fbe5
AhmedRezeq8 <sherlock.am8@gmai.com>
2023-09-18 12:04:28 +0300

    Manage Members

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -19,8 +20,8 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
-      {/* <MembersModal />
-      <CreateChannelModal />
+      <MembersModal />
+      {/* <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
